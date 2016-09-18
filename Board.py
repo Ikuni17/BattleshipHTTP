b1d521b2b7ef772c69a5a274d9009ab073501949
Jason <J_C_Sanders@hotmail.com>
2016-09-18 13:26:23 -0600

    Made a folder for board files. Streamlined player/enemy board creation. Added a preemptive method to check if a coordinate has been fired at. Added a method to check for sunken ships.

--- Board.py
+++ Board.py
@@ -1,52 +1,55 @@
 import random
 
+#Global Variables
+board = dict([(0, []), (1, []), (2, []), (3, []), (4, []), (5, []), (6, []), (7, []), (8, []), (9, [])])
+eboard = dict([(0, []), (1, []), (2, []), (3, []), (4, []), (5, []), (6, []), (7, []), (8, []), (9, [])])
+counters = [0, 0, 0, 0, 0]
+
+
 def main():
+	global board
+	global eboard
+	
 	print("Making Board...")
-	board = dict()
-	make_board(board)
+	make_board()
 	print()
+	
 	print("Making Enemy Board...")
-	eboard = dict()
-	make_enemy(eboard)
+	make_opponent()
 	print()
 
-	check_for_hit(2, 7)
-	check_for_hit(1, 3)
-	check_for_hit(4, 9)
-	check_for_hit(5, 5)
+	if useCoord((2, 7)) == False:
+		check_for_hit((2,7))
+	if useCoord((1, 3)) == False:
+		check_for_hit((1,3))
+	if useCoord((4, 9)) == False:
+		check_for_hit((4,9))
+	if useCoord((5, 5)) == False:
+		check_for_hit((5,5))
 
-	update_eboard(0,0,0)
-	update_eboard(2,5,1)
-	update_eboard(6,7,8)
+	update_eboard((0,0),0)
+	update_eboard((2,5),1)
+	update_eboard((2,4),1)
 
-def make_board(board):
-	rows = 10
-	cols = 10
-	board = dict([(0, []), (1, []), (2, []), (3, []), (4, []), (5, []), (6, []), (7, []), (8, []), (9, [])])
-	for i in range(0,cols):
-		for j in range(0,rows):
+def make_board():
+	global board
+
+	for i in range(0,len(board)):
+		for j in range(0,10):
 			board[i].append("_")
-	place_ship(board, "Carrier")
-	place_ship(board, "Battleship")	
-	place_ship(board, "Cruiser")	
-	place_ship(board, "Submarine")
-	place_ship(board, "Destroyer")
+	
+	place_ship("Carrier")
+	place_ship("Battleship")	
+	place_ship("Cruiser")	
+	place_ship("Submarine")
+	place_ship("Destroyer")
 
-	print_board(board)
-	write_board(board)
+	print_board()
+	write_board()
 
-def make_enemy(eboard):
-	rows = 10
-	cols = 10
-	board = dict([(0, []), (1, []), (2, []), (3, []), (4, []), (5, []), (6, []), (7, []), (8, []), (9, [])])
-	for i in range(0,cols):
-		for j in range(0,rows):
-			board[i].append("_")
-			print(board[i][j], end="")
-		print()
-	write_eboard(eboard)
+def place_ship(shipType):
+	global board
 
-def place_ship(board, shipType):
 	curX = random.randint(0,9)
 	curY = random.randint(0,9)
 	orient = random.randint(0,1)
@@ -106,56 +109,128 @@ def place_ship(board, shipType):
 				curX = random.randint(0,9)
 				curY = random.randint(0,9)
 
-def print_board(board):
+def print_board():
+	global board
+
 	for i in range(0,len(board)):
 		for j in range(0,len(board[i])):
 			print(board[i][j], end="")
 		print()
 
-def write_board(board):
-	boardFile = open('Boards/player_board.txt', 'w')
+def write_board():
+	global board
+
+	boardFile = open('Boards/own_board.txt', 'w')
 	for i in range(0,len(board)):
 		for j in range(0,len(board[i])):
 			boardFile.write(board[i][j])
 		boardFile.write('\n')
 	boardFile.close()
 
-def write_eboard(eboard):
-	eboardFile = open('Boards\enemy_board.txt', 'w')
-	for i in range(0,9):
-		for j in range(0,9):
-			eboardFile.write("_")
+def useCoord(coord):
+	global board
+
+	if board[coord[1]][coord[0]] is "1" or board[coord[1]][coord[0]] is "0":
+		return True
+	else:
+		return False
+
+def check_for_hit(coord):
+	global board
+	global counters
+
+	print("Checking " + str(coord) + ".")
+	if board[coord[1]][coord[0]] == "_":
+		print("Miss at point " + str(coord) + "!")
+		board[coord[1]][coord[0]] = "0"
+		write_board()
+		return (0)
+	else:
+		print("Hit at point " + str(coord) + "!")
+		title = board[coord[1]][coord[0]]
+		if title is "C":
+			#print(title)
+			counters[0] += 1
+		elif title is "B":
+			#print(title)
+			counters[1] += 1
+		elif title is "R":
+			#print(title)
+			counters[2] += 1
+		elif title is "S":
+			#print(title)
+			counters[3] += 1
+		elif title is "D":
+			#print(title)
+			counters[4] += 1
+		else:
+			print("You hit a ghost ship...")
+		return check_for_sink(coord, title)
+
+def make_opponent():
+	global ebaord
+
+	for i in range(0,len(eboard)):
+		for j in range(0,10):
+			eboard[i].append("_")
+
+	print_opponent()
+	write_opponent()
+
+def print_opponent():
+	global eboard
+
+	for i in range(0,len(eboard)):
+		for j in range(0,len(eboard[i])):
+			print(board[i][j], end="")
+		print()
+
+def write_opponent():
+	global eboard
+
+	eboardFile = open("Boards/opponent_board.txt", "w")
+	for i in range(0,len(eboard)):
+		for j in range(0,len(eboard[i])):
+			eboardFile.write(eboard[i][j])
 		eboardFile.write('\n')
 	eboardFile.close()
 
-def check_for_hit(x, y):
-	print("Checking (" + str(y) + ", " + str(x) + ")")
-	board = open('Boards\player_board.txt', 'r')
-	data = board.read()
-	board.close()
-	
-	rows = data.split('\n')
-	if rows[y][x] == "_":
-		print("Miss at point (" + str(y) + ", " + str(x) + ")!")
-		rows[y][x] = "0"
-	elif rows[y][x] == "0" || rows[y][x] == "1" :
-		print("This point's been tried already.")
+def update_eboard(coord, result):
+	global eboard
+
+	if result == 0:
+		print("You missed the enemy at point " + str(coord) + "!")
+		eboard[coord[1]][coord[0]] = "0"
 	else:
-		print("Hit at point (" + str(y) + ", " + str(x) + ")!")
-		check_for_sink(x, y, rows)
+		print("You hit the enemy at point " + str(coord) + "!")
+		eboard[coord[1]][coord[0]] = "1"
 
-def update_eboard(x, y, pwr):
-	data = open("Boards\enemy_board.txt", "read")
-	eboard = data.read()
-	rows = data.split('\n')
-	rows[y][x] = pwr
-	if pwr == 0:
-		print("You missed the enemy at point (" + str(y) +", " + str(x) + ")")
+	write_opponent()
+
+def check_for_sink(coord, title):
+	global board
+	global counters
+
+	board[coord[1]][coord[0]] = "1"
+	write_board()
+
+	if counters[0] == 5:
+		#print("There's a sunken Carrier!")
+		return (1,"C")
+	elif counters[1] == 4:
+		#print("There's a sunken Battleship!")
+		return (1,"B")
+	elif counters[2] == 3:
+		#print("There's a sunken Cruiser!")
+		return(1,"R")
+	elif counters[3] == 3:
+		#print("There's a sunken Submarine!")
+		return(1,"S")
+	elif counters[4] == 2:
+		#print("There's a sunken Destroyer!")
+		return(1,"D")
 	else:
-		print("You hit the enemy at point (" + str(y) +", " + str(x) + ")")
+		#print("No sink!")
+		return (1)
 
-
-def check_for_sink(x, y, rows):
-	title = rows[y][x]
-	rows[y][x] = 1
 main()
